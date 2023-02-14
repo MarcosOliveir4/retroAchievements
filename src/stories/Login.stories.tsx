d9a7443858116534722036383d9a122a90cfaec9
MarcosOliveir4 <marcosassis071@gmail.com>
2023-02-14 16:05:45 -0300

    fix: ajustando imports para o storybook

--- src/stories/Login.stories.tsx
+++ src/stories/Login.stories.tsx
@@ -1,11 +1,16 @@
-import { Login } from '../components/Login';
+import { Login } from '@/components/Login';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { AuthProvider } from '@/context/AuthContext';
 
 export default {
   title: 'Components/Login',
   component: Login,
 } as ComponentMeta<typeof Login>;
 
-const Template: ComponentStory<typeof Login> = () => <Login />;
+const Template: ComponentStory<typeof Login> = () => (
+  <AuthProvider>
+    <Login />
+  </AuthProvider>
+);
 
 export const Default = Template.bind({});
